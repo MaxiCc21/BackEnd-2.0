ea4ba2e4c911c39360a39c71db0bff979fcf45a4
MaxiCc21 <maxi21498@gmail.com>
2024-06-04 18:23:59 -0300

    feat: :sparkles: Recover-Password
    1.Verifica si el usurio existe 2.Manda el mail(que dura un itempo x) 3.Cambia la contraseña

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,243 +1,257 @@
 const { Router } = require("express");
 const UserModel = require("../models/user.model");
 const { hashPassword, comparePassword } = require("../utils/bcrypt");
+const nodemailer = require("nodemailer");
+const { sendMail, sendPasswordResetEmail } = require("../utils/sendMail");
 
+const router = Router();
 
-const router = Router()
+router.get("/acceso", (req, res) => {
+  const options = {
+    title: "Usuario",
+  };
 
+  res.render("user/user_login", options);
+});
 
-router.get("/acceso",(req,res) => {
+router.post("/acceso", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-    const options = {
-        title:"Usuario"
+    const userExist = await UserModel.findOne({ email });
+
+    if (!userExist) {
+      return res.status(400).send({
+        status: "Error",
+        ok: false,
+        statusMessage: "Alguno de los datos es incorrecto",
+      });
     }
-    
-    res.render("user/user_login",options)
-})
 
+    if (await comparePassword(password, userExist.password)) {
+      req.session.user = {
+        id: userExist._id,
+        email: userExist.email,
+        name: userExist.name,
+      };
 
-router.post("/acceso", async (req,res) => {
-    
-    try {
-        const {email,password} = req.body
-
-        const userExist = await UserModel.findOne({email})
-    
-        if (!userExist) {
-            return res.status(400).send({
-                status:"Error",
-                ok:false,
-                statusMessage:"Alguno de los datos es incorrecto"
-            })
-        }
-
-
-        if ( await comparePassword(password,userExist.password)) {
-
-            req.session.user = {
-                id: userExist._id,
-                email: userExist.email,
-                name: userExist.name
-            }
-
-            return res.send({
-                status:"ok",
-                ok:true,
-                statusMessage:"Bienvenido"
-            })
-        }else{
-            return res.status(400).send({
-                status:"Error",
-                ok:false,
-                statusMessage:"Alguno de los datos es incorrecto"
-            })
-        }
-    } catch (error) {
-            return res.status(500).send({
-                status:"Error",
-                ok:false,
-                statusMessage:"Ocurrio un error inesperado /n Por favor intente nuevamente mas tarde"
-            })
+      return res.send({
+        status: "ok",
+        ok: true,
+        statusMessage: "Bienvenido",
+      });
+    } else {
+      return res.status(400).send({
+        status: "Error",
+        ok: false,
+        statusMessage: "Alguno de los datos es incorrecto",
+      });
     }
-})
+  } catch (error) {
+    return res.status(500).send({
+      status: "Error",
+      ok: false,
+      statusMessage:
+        "Ocurrio un error inesperado /n Por favor intente nuevamente mas tarde",
+    });
+  }
+});
 
+router.get("/registro", (req, res) => {
+  const options = {
+    title: "Crear Usuario",
+  };
 
-router.get("/registro",(req,res) => {
-    const options = {
-        title:"Crear Usuario"
-    }
-    
-
-    res.render("user/user_register",options)
-})
-
+  res.render("user/user_register", options);
+});
 
 router.post("/registro", async (req, res) => {
-    try {
-        const { email } = req.body;
+  try {
+    const { email } = req.body;
 
-        let newUserData = {
-            ...req.body,
-            password: "Pass1234"
-        };
+    let newUserData = {
+      ...req.body,
+      password: "Pass1234",
+    };
 
-        const encryptedPassword = await hashPassword(newUserData.password)
+    const encryptedPassword = await hashPassword(newUserData.password);
 
-        newUserData = {
-            ...req.body,
-            password: encryptedPassword
-        };
+    newUserData = {
+      ...req.body,
+      password: encryptedPassword,
+    };
 
-        const createNewUser = await UserModel.findOneAndUpdate(
-            { email },
-            newUserData,
-            { upsert: true, new: true }
-        );
+    const createNewUser = await UserModel.findOneAndUpdate(
+      { email },
+      newUserData,
+      { upsert: true, new: true }
+    );
 
-
-        if (createNewUser) {
-            res.send("Usuario creado correctamente");
-        } else {
-                console.error('El Usuario ya existe');
-                res.status(500).send("El Usuario ya existe");
-        }
-    } catch (error) {
-        console.error('Error al crear usuario:', error);
-        res.status(500).send("Error al crear usuario");
+    if (createNewUser) {
+      res.send("Usuario creado correctamente");
+    } else {
+      console.error("El Usuario ya existe");
+      res.status(500).send("El Usuario ya existe");
     }
+  } catch (error) {
+    console.error("Error al crear usuario:", error);
+    res.status(500).send("Error al crear usuario");
+  }
 });
 
+router.delete("/registro/:email", async (req, res) => {
+  try {
+    // const {email} = req.body
+    const userEmail = req.params.email;
 
-router.delete("/registro/:email",async (req,res) => {
-    
-    try {
-        // const {email} = req.body
-        const userEmail = req.params.email;
+    const deleteUser = await UserModel.findOneAndDelete({ email: userEmail });
 
-        const deleteUser = await UserModel.findOneAndDelete({email:userEmail})
-        
-        if (deleteUser.deletedCount != 1) {
-            res.send("Usuario eliminado correctamente");
-        } else {
-            res.status(404).send("Usuario no encontrado");
-        }
-
-    } catch (error) {
-        console.error('Error al eliminar usuario:', error);
-        res.status(500).send("Error al eliminar usuario: " + error.message);
+    if (deleteUser.deletedCount != 1) {
+      res.send("Usuario eliminado correctamente");
+    } else {
+      res.status(404).send("Usuario no encontrado");
     }
-
-})
-
+  } catch (error) {
+    console.error("Error al eliminar usuario:", error);
+    res.status(500).send("Error al eliminar usuario: " + error.message);
+  }
+});
 
 router.get("/logout", (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return res.status(500).send("Error al cerrar sesión");
-        }
-        res.redirect("/home");
-    });
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send("Error al cerrar sesión");
+    }
+    res.redirect("/home");
+  });
 });
 
+router.get("/userdata", async (req, res) => {
+  try {
+    const userSession = req.session.user || undefined;
+    const ID = userSession.id;
 
-router.get("/userdata",async (req,res) => {
-    
+    const userData = await UserModel.findOne({ _id: ID }).lean();
 
-    try {
-        const userSession = req.session.user || undefined
-        const ID = userSession.id
+    const options = {
+      title: "Datos de usuario",
+      userData,
+      userSession,
+    };
 
-        const userData = await UserModel.findOne({_id:ID}).lean();
-            
-    
-        const options = {
-            title:"Datos de usuario",
-            userData,
-            userSession
-        }
-    
-        res.render("user/userData",options) 
-    } catch (err) {
-        res.status(500).send("Algo salio mal")
-    }
-})
-
+    res.render("user/userData", options);
+  } catch (err) {
+    res.status(500).send("Algo salio mal");
+  }
+});
 
 router.get("/recuperar-contrasena", async (req, res) => {
-
-    res.render("user/recoverPassword")
-})
+  res.render("user/recoverPassword");
+});
 
 router.post("/recuperar-contrasena", async (req, res) => {
+  try {
+    let { email_recoverPassword } = req.body;
 
-    try {
-        
-        let {email_recoverPassword,new_password} = req.body
+    const findUpdatePassword = await UserModel.findOne({
+      email: email_recoverPassword,
+    });
 
+    if (!findUpdatePassword) {
+      res.status(400).send({
+        status: 400,
+        ok: false,
+        error: false,
+        stateMsj: "No se ha encontrado un suario con ese email",
+        data: null,
+      });
+    } else {
+      const enviarEmail = await sendPasswordResetEmail(email_recoverPassword);
 
-        const newPasswordHashed =  await hashPassword(new_password)
+      if (enviarEmail) {
+        res.status(200).send({
+          status: 200,
+          ok: true,
+          error: false,
+          stateMsj: "Se le envio un mensaje a su correo electronico",
+          data: null,
+        });
+      } else {
+        res.status(400).send({
+          status: 400,
+          ok: false,
+          error: true,
+          stateMsj: "Ocurrio un error inesperado al inviar el email",
+          data: null,
+        });
+      }
+    }
+  } catch (err) {
+    console.log(err);
 
-        const findUpdatePassword = await UserModel.findOneAndUpdate({email:email_recoverPassword},{password:newPasswordHashed})
+    res.status(500).send({
+      status: 500,
+      ok: false,
+      error: err,
+      stateMsj: "Ah ocurrido un error inesperado",
+      data: null,
+    });
+  }
+});
 
-        if (!findUpdatePassword) {
+router.get("/cambiar-contrasena", async (req, res) => {
+  try {
+    const { token } = req.query;
+    const user = await UserModel.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() },
+    });
 
-            res.status(400).send({
-                status: 400,
-                ok: false,
-                error: false,
-                statusMessage: "No se ha encontrado un suario con ese email",
-                data: null,
-            })
-            // return {
-            //     status: 400,
-            //     ok: false,
-            //     error: false,
-            //     statusMessage: "No se ha encontrado un suario con ese email",
-            //     data: null,
-            // };
-
-        } else {
-
-
-            res.status(200).send({
-                status: 200,
-                ok: true,
-                error: false,
-                statusMessage: "Contraseña actualizada con exito",
-                data: null,
-            })
-
-
-            // return {
-            //     status: 200,
-            //     ok: true,
-            //     error: false,
-            //     statusMessage: "Contraseña actualizada con exito",
-            //     data: null,
-            // };
-        }
-
-    } catch (err) {
-        console.log(err);
-
-        res.status(500).send({
-            status: 500,
-            ok: false,
-            error: err,
-            stateMsj: "Ah ocurrido un error inesperado",
-            data: null,
-        })
-
-        // return {
-        //     status: 500,
-        //     ok: false,
-        //     error: err,
-        //     stateMsj: "Ah ocurrido un error inesperado",
-        //     data: null,
-        // };
+    if (!user) {
+      return res
+        .status(400)
+        .send(
+          "El enlace de restablecimiento de contraseña es inválido o ha expirado."
+        );
     }
 
+    res.render("user/changePassword");
+  } catch (err) {
+    res.status(500).send("Ocurrió un error");
+  }
+});
 
+router.post("/cambiar-contrasena", async (req, res) => {
+  try {
+    const { token, newPassword } = req.body;
 
-})
-module.exports = router
+    if (!token) {
+      return res.status(400).send("Token no proporcionado.");
+    }
+
+    const user = await UserModel.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .send(
+          "El enlace de restablecimiento de contraseña es inválido o ha expirado."
+        );
+    }
+
+    user.password = await hashPassword(newPassword);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+    await user.save();
+
+    res.send("Contraseña restablecida con éxito.");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Ocurrió un error.");
+  }
+});
+
+module.exports = router;
