327f5ccd375f8fc243d688a655c6e8e95cfd14e5
MaxiCc21 <maxi21498@gmail.com>
2024-06-17 18:33:40 -0300

    .

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,11 +1,48 @@
-// Datos para las opciones del dropdown
-// const options = ["Buenos Aires Aeroparque", "Buenos Aires Ezeiza", "Cordoba", "Salta"];
+document.addEventListener("DOMContentLoaded", () => {
+  const oneWayCheckbox = document.getElementById("oneWay");
+  const roundTripCheckbox = document.getElementById("roundTrip");
 
-// Obtener elementos del DOM
-const searchInput = document.getElementById("searchInput");
-const searchDropdown = document.getElementById("searchDropdown");
+  const searchInput = document.getElementById("searchInput");
+  const searchDropdown = document.getElementById("searchDropdown");
 
-// Filtrar y obtener solo los valores de la clave "province"
+  const labelReturnDateFlight = document.getElementById(
+    "label-returnDateFlight"
+  );
+  const returnDateFlightDIV = document.getElementById(
+    "returnDateFlightContainer"
+  );
+
+  oneWayCheckbox.addEventListener("change", function () {
+    if (!this.checked && !roundTripCheckbox.checked) {
+      this.checked = true;
+    } else {
+      roundTripCheckbox.checked = false;
+
+      returnDateFlightDIV.style.display = "none";
+    }
+  });
+
+  roundTripCheckbox.addEventListener("change", function () {
+    if (!this.checked && !oneWayCheckbox.checked) {
+      this.checked = true;
+    } else {
+      oneWayCheckbox.checked = false;
+      returnDateFlightDIV.style.display = "flex";
+    }
+  });
+
+  const form = document.getElementById("searchFlyForm");
+  const returnDateFlightInput = document.getElementById("returnDateFlight");
+
+  form.addEventListener("submit", function (event) {
+    if (!returnDateFlightInput.value) {
+      returnDateFlightInput.disabled = true;
+    } else {
+      returnDateFlightInput.disabled = false;
+    }
+  });
+});
+
 const provincesArray = options.map((item) => item.province);
 
 // Mostrar el dropdown cuando se escriba en el input
